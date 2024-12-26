b4c144b8e5421ec4760c770e89925e506a43e306
LeeNux <lc7457@qq.com>
2024-12-26 16:57:39 +0800

    更新axios版本

--- adapters/uniapp.js
+++ adapters/uniapp.js
@@ -1,16 +1,15 @@
+import settle from "axios/unsafe/core/settle.js"
+import buildURL from "axios/unsafe/helpers/buildURL.js"
+import buildFullPath from "axios/unsafe/core/buildFullPath.js"
+
 export default function adapter(config) {
     return new Promise((resolve, reject) => {
-        let settle = require('axios/lib/core/settle');
-        let buildURL = require('axios/lib/helpers/buildURL');
-        let buildFullPath = require('axios/lib/core/buildFullPath');
-
         let fullUrl = buildFullPath(config.baseURL, config.url)
-
         uni.request({
             method: config.method.toUpperCase(),
             url: buildURL(fullUrl, config.params, config.paramsSerializer),
             header: config.headers,
-            data: config.data,
+            data: typeof config.data === 'object' ? JSON.stringify(config.data) : config.data,
             dataType: config.dataType,
             responseType: config.responseType,
             sslVerify: config.sslVerify,
